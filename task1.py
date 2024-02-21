58ba4aeb34e21d91f5f16e9eabff9189bcfc8986
Magiceee <magicice537@gmail.com>
2024-02-21 16:44:55 +0300

    2lab

---
+++ task1.py
@@ -0,0 +1,4 @@
+password1 = input("введите пароль")
+password2 = input("повторите пароль")
+if password2 == password1: print("пароль верный")
+else: print("пароль неверный")
---
+++ task2.py
@@ -0,0 +1,5 @@
+seatnumber = int(input("введите номер места в вагоне"))
+if seatnumber >= 37: print("боковое")
+else: print("купе")
+if seatnumber % 2 == 1: print("нижнее")
+else: print("верхнее")
